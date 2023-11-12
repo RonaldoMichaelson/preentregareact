f6ee79c0db3ba45b6194d276840ba9b8b6ec446d
Ronaldo <102784239+RonaldoMichaelson@users.noreply.github.com>
2023-11-12 15:25:52 -0800

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/2019_Chevrolet_Camaro_base%2C_front_11.9.19.jpg/1200px-2019_Chevrolet_Camaro_base%2C_front_11.9.19.jpg',
+		nameProduct: 'Camaro',
+		price: 300000,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://acnews.blob.core.windows.net/imgnews/medium/NAZ_82afbbae82664a82a0f2cab7e0ccf7c2.webp	',
+		nameProduct: 'Lamborghini',
+		price: 250000,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://e00-expansion.uecdn.es/assets/multimedia/imagenes/2022/05/20/16530388291489.jpg',
+		nameProduct: 'Ferrari',
+		price: 300000,
+		quantity: 1,
+	},
+	
+];
