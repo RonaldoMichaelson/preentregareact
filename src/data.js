a1374fbc8e7dc82b2a8fd4e25a1d04a643fcd0db
RonaldoMichaelson <ronaldomichaelson1@gmail.com>
2023-11-12 15:03:52 -0800

    grabacion

--- src/data.js
+++ src/data.js
@@ -1,38 +1,24 @@
 export const data = [
 	{
 		id: 1,
-		img: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-		nameProduct: 'Zapatos Nike',
-		price: 80,
+		img: 'https://discouy.vtexassets.com/arquivos/ids/1524417/Banana-Brasil-apro-x-150-g-0.jpg?v=638339333070400000',
+		nameProduct: 'Bananas',
+		price: 300,
 		quantity: 1,
 	},
 	{
 		id: 2,
-		img: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80',
-		nameProduct: 'Reloj',
-		price: 50,
+		img: 'https://discouy.vtexassets.com/arquivos/ids/1524613/Frutilla-especial-0.jpg?v=638339333190800000',
+		nameProduct: 'Frutillas',
+		price: 250,
 		quantity: 1,
 	},
 	{
 		id: 3,
-		img: 'https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
-		nameProduct: 'Smartwatch',
-		price: 90,
+		img: 'https://discouy.vtexassets.com/arquivos/ids/1524502-800-auto?v=638339333124600000&width=800&height=auto&aspect=true',
+		nameProduct: 'Kiwi',
+		price: 399,
 		quantity: 1,
 	},
-	{
-		id: 4,
-		img: 'https://images.unsplash.com/photo-1585386959984-a4155224a1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-		nameProduct: 'Perfume',
-		price: 50,
-		quantity: 1,
-	},
-	{
-		id: 5,
-		img: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-		nameProduct: 'Audifonos',
-		price: 20,
-		quantity: 1,
-	},
-	// Aquí pueden agregar más o modificar los productos existentes
+	
 ];
